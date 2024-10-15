799420d93bc36d3e539b7e713622ccc1c222f8ca
Diwanshu Gangwar <118631453+DiwanshuG@users.noreply.github.com>
2024-10-15 23:37:44 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import TicTacToe from "./components/tic-tac-toe";
+function App() {
+  return (
+    <div className="App">
+      {/* Tic-tac-toe component  */}
+
+      <TicTacToe/>
+    </div>
+  );
+}
+
+export default App;
